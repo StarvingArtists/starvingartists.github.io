4603b8d435b7c27c8efb0d9d009ff81383567860
StarvingArtists <101067392+StarvingArtists@users.noreply.github.com>
2022-03-06 22:24:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,15 @@ $(window).on("load resize ", function() {
   var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
   $('.tbl-header').css({'padding-right':scrollWidth});
 }).resize();
+
+let dropdown = $('#locality-dropdown');
+dropdown.empty();
+
+dropdown.append('<option selected="true" disabled>Choose State/Province</option>');
+dropdown.prop('selectedIndex', 0);
+
+$.getJSON("values.json", function (data) {
+  $.each(data, function (key, entry) {
+    dropdown.append($('<option></option>').attr('value', entry.abbreviation).text(entry.name));
+  })
+});
